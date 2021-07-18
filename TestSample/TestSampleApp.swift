7ae906dd14d69a6db39f879b7fcce1bac3a0cd3c
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-19 08:40:52 +0900

    Initial Commit

---
+++ TestSample/TestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSampleApp.swift
+//  TestSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct TestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
